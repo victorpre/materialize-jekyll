dec5aaa4bb1e720f82bb2327e5969bc429c03fd6
Victor Presumido <victor.presumido@gmail.com>
2017-08-26 18:50:25 -0300

    🌟 JS filtering

--- assets/js/init.js
+++ assets/js/init.js
@@ -1,14 +1,38 @@
 (function($){
   $(function(){
-    //$('.button-collapse').sideNav();
-    //$('.parallax').parallax();
-
     // Masonry Grid
     var $container = $('#masonry-grid');
+
     // initialize
     $container.masonry({
-      columnWidth: '.col',
-      itemSelector: '.col',
+      columnWidth: '.col:not([style*="display: none"])',
+      itemSelector: '.card-wrapper',
+      horizontalOrder: true
+    });
+
+    // Show/reveal cards
+    $(".tag-name").click(function(el){
+      el.preventDefault();
+      const tag = $(this).data("tag");
+      var $chips = $("[data-tag="+tag+"] > .chip");
+      var $cards = {};
+      if ($chips.hasClass("active")){
+        $chips.toggleClass("active");
+        if ($(".active").size()){
+          $cards = $('.card-wrapper[style*="display: none"]').filter(function() {
+            return $(".active", this).size();
+          });
+        }else{
+          $cards = $('.card-wrapper[style*="display: none"]');
+        }
+      }else{
+        $chips.toggleClass("active");
+        $cards = $('.card-wrapper:not([style*="display: none"])').not(function() {
+          return $("[data-tag="+tag+"]", this).size();
+        });
+      }
+      $cards.toggle();
+      $container.masonry('layout').masonry();
     });
 
     // Scroll Reveal
@@ -16,12 +40,12 @@
     sr.reveal('.card');
 
     // Mobile button colapse
-	function adjustIndex() {
+    function adjustIndex() {
       $('body').scrollTop(0);
       $("#sidenav-overlay").addClass("adjust-index");
     }
 
-	$(".button-collapse").sideNav({
+    $(".button-collapse").sideNav({
       menuWidth: 1000, // Default is 240
       edge: 'left', // Choose the horizontal origin
       closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
